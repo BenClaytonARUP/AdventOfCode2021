c124c5fda3692b82ef7c45821f25fbc6d58060a5
GLOBAL\Ben.Clayton <ben.clayton@arup.com>
2021-12-21 15:41:14 +0000

    day6 done

--- python/day6.py
+++ python/day6.py
@@ -1,12 +1,14 @@
 import re
 import os
 import numpy as np
+from time import time
 
 
 def main():
 
     Part1()
     Part2()
+    CompareApproaches()
 
 def Part1():
 
@@ -20,12 +22,62 @@ def Part1():
 
     print(len(s.schoolFish))
 
+def Part2():
+
+    with open(os.path.join("..", "data", "6.txt"), "r") as f:
+        inputData = [int(i) for i in f.readline().split(",")]    
+
+    eS = efficientSchool(inputData)
+
+    for i in range(256):
+        eS.update()
+
+    print(eS.currentSchool.sum())
+
+def CompareApproaches():    
+
+    with open(os.path.join("..", "data", "6.txt"), "r") as f:
+        inputData = [int(i) for i in f.readline().split(",")]    
+
+    N = 10
+
+    # Simple object orientated
+    tA1 = time()
+    for k in range(N):
+        s = School(inputData)
+        for i in range(80):
+            s.update()
+    tA2 = time()
+    print(len(s.schoolFish), "TIME: ", tA2 - tA1)
+
+    # numpy
+    tB1 = time()
+    for k in range(N):
+        data = np.array(inputData, dtype=int)
+        for i in range(80):
+            data = Update(data)
+    tB2 = time()
+    print(data.shape[0], "TIME: ", tB2 - tB1)
+
+    # efficientSchool
+    tC1 = time()
+    for k in range(N):
+        eS = efficientSchool(inputData)
+        for i in range(80):
+            eS.update()
+    tC2 = time()
+    print(eS.currentSchool.sum(), "TIME: ", tC2 - tC1)
+    
+
+
+
+
 class School:    
 
     def __init__(self, inputData):
+        self.schoolFish = []
         
         for timerCount in inputData:
-            self.schoolFish = []
             self.schoolFish.append(Fish(timerCount))
 
     def update(self):
@@ -47,30 +99,22 @@ class Fish:
             return True
         self.timerCount -= 1
 
-def Part2():
 
-    with open(os.path.join("..", "data", "6.txt"), "r") as f:
-        inputData = [int(i) for i in f.readline().split(",")]
 
-    data = np.array(inputData)
+class efficientSchool:
 
-    for i in range(256):
-        print("Day", i+1)
-        data = Update(data)
+    def __init__(self, inputData):
+        self.currentSchool = np.zeros((9), dtype=np.dtype(np.int64))
+        for timerCount in inputData:
+            self.currentSchool[timerCount] += 1
 
-    print(len(data))
-
-    print("HERE")
-    quit()
-
-    s2 = School(inputData)
-
-    for i in range(256):
-        print("Day", i+1)
-        s2.update()
-
-    print(len(s2.schoolFish))
-
+    def update(self):
+        zeroFish = self.currentSchool[0]
+        for i in range(self.currentSchool.shape[0]-1):
+            self.currentSchool[i] = self.currentSchool[i+1]
+        self.currentSchool[6] += zeroFish
+        self.currentSchool[8] = zeroFish
+            
 
 def Update(data):
 
